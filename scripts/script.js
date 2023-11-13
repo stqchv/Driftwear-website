7d1dcf981cc9b081506ad9d0a93ddf2832e4167b
stqchv <148893477+stqchv@users.noreply.github.com>
2023-11-13 11:06:09 +0100

    Adding pop-up animations

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,44 @@
-/* ---------- Closing menu ---------- */
-
-let menu = document.querySelector('#menu-icon');
-let nav__link = document.querySelector('.nav__link');
-
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    nav__link.classList.toggle('open');
-}
-
-
-/* ---------- Scroll ---------- */
+/* ---------- Scroll navbar ---------- */
 const body = document.body;
-        let lastScroll = 0;
+let lastScroll = 0;
 
-        window.addEventListener('scroll', () => {
-            const currentScroll = window.pageYOffset
+window.addEventListener('scroll', () => {
+    const currentScroll = window.pageYOffset
             
-            if (currentScroll <= 0) {
-                body.classList.remove("scroll-up")
-            }
+    if (currentScroll <= 0) {
+        body.classList.remove("scroll-up")
+    }
 
-            if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
-                body.classList.remove("scroll-up")
-                body.classList.add("scroll-down")
-            }
+    if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+        body.classList.remove("scroll-up")
+        body.classList.add("scroll-down")
+    }
 
-            if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
-                body.classList.remove("scroll-down")
+    if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
+        body.classList.remove("scroll-down")
                 body.classList.add("scroll-up")
-            }
+    }
 
-            lastScroll = currentScroll;
-        })
+    lastScroll = currentScroll;
+})
+
+/* ---------- Pop-up animations ---------- */
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+    var reveals = document.querySelectorAll('.reveal');
+
+    for(var i = 0; i < reveals.length; i++){
+        
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if(revealtop <windowheight - revealpoint){
+            reveals[i].classList.add('active');
+        }
+        else{
+            reveals[i].classList.remove('active');
+        }
+    }
+}
